af920d10e3a5581275eff2d9fe01766a715d6b34
unknown <mateusz.lu123@gmail.com>
2025-09-21 15:30:23 +0200

    added link to job opening

--- obj/Debug/net9.0/JobSearcher.AssemblyInfo.cs
+++ obj/Debug/net9.0/JobSearcher.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JobSearcher")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33e03c9dc46d02ddecfdcd308df91f90e1001a27")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b3b38b5f3e2bda570372602674d046aa92af9b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("JobSearcher")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JobSearcher")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
