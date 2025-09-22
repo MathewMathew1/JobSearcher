ed661e7f48d019199bddd2ab6c7fc86c168b1e9c
unknown <mateusz.lu123@gmail.com>
2025-09-22 12:06:45 +0200

    added constant helper

---
+++ Constant/ConstantHelper.cs
@@ -0,0 +1,8 @@
+namespace JobSearcher.Constant
+{
+    public static class ConstantHelpers
+    {
+        public const int NumberOfSearches = 3;
+        public const int NumberOfHours = 2; 
+    }
+}
