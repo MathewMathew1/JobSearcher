1ac165520186ca07921e9c2f18af7c967f067bc4
unknown <mateusz.lu123@gmail.com>
2025-09-30 17:47:04 +0200

    removed not used imports

--- Report/ReportsBackgroundService.cs
+++ Report/ReportsBackgroundService.cs
@@ -1,10 +1,3 @@
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Threading;
-using System.Threading.Tasks;
-
 namespace JobSearcher.Report
 {
     public class ReportSetupBackgroundService : BackgroundService
